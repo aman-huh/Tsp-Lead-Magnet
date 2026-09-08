71897f6860651b4d06bb3a8d87e5ed3450f2a783
aman-huh <aman.g@thumbstack.co>
2026-09-08 22:35:03 +0530

    feat: add cloudinary upload provider

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -31,8 +31,19 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
       },
     },
   },
-  upload: {
+    upload: {
     config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
       security: {
         allowedTypes: allowedMediaTypes,
         deniedTypes,
